777ef4f02fc8ccddf95ffd28a9483974aba1ee8f
Oleksandr Bodnarchuk <oleksandr.bodnarchuk@atos.net>
2025-07-14 20:53:19 +0200

    discovery-service - init

---
+++ src/test/java/pl/alex/app/discovery/AppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.alex.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
